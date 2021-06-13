53a2896566b89661aaff8ad08d68f44cea92031e
Jeremie Jost <jeremiejost@gmail.com>
2021-06-13 19:20:46 +0200

    Forgot to update dist

--- dist/index.js
+++ dist/index.js
@@ -5697,11 +5697,31 @@ isStream.transform = function (stream) {
 
 const core = __webpack_require__(470);
 const exec = __webpack_require__(986);
+__webpack_require__(530);
 
+Promise.retry = function(fn, times, delay) {
+    return new Promise(function(resolve, reject){
+        var error;
+        var attempt = function() {
+            if (times == 0) {
+                reject(error);
+            } else {
+                fn().then(resolve)
+                    .catch(function(e){
+                        times--;
+                        error = e;
+                        setTimeout(function(){attempt()}, delay);
+                    });
+            }
+        };
+        attempt();
+    });
+};
 
 async function run() {
   try {
-    __webpack_require__(530);
+    // Hack: wait for doctl to get installed
+    await Promise.retry(() => exec.exec('doctl', [], {'silent': true}), 30, 1000);
     var applicationId = core.getInput('application-id');
     var specPath = core.getInput('spec');
     await exec.exec('doctl apps update', [applicationId, '--spec', specPath]);
