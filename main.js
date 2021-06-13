ea48a4cee87cac0735f648aec3a76bf3935b1e23
Jeremie Jost <jeremiejost@gmail.com>
2021-06-13 19:15:35 +0200

    Hack: wait for doctl install to finish

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
+require('action-doctl');
 
+Promise.retry = function(fn, times, delay) {
+    return new Promise(function(resolve, reject){
+        var error;
+        var attempt = function() {
+            if (times == 0) {
+                reject(error);
+            } else {
+                fn().then(resolve)
+                    .catch(function(e){
+                        times--;
+                        error = e;
+                        setTimeout(function(){attempt()}, delay);
+                    });
+            }
+        };
+        attempt();
+    });
+};
 
 async function run() {
   try {
-    require('action-doctl');
+    // Hack: wait for doctl to get installed
+    await Promise.retry(() => exec.exec('doctl', [], {'silent': true}), 30, 1000);
     var applicationId = core.getInput('application-id');
     var specPath = core.getInput('spec');
     await exec.exec('doctl apps update', [applicationId, '--spec', specPath]);
