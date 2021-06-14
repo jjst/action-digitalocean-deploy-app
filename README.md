aedef07b48a63b5fe3439f0c22081caeed34e7d3
Jérémie Jost <jeremiejost@gmail.com>
2021-06-14 20:20:24 +0200

    New action version, support creating app if it doesn't exist (#1)
    * Workflow to test action
    
    * Provide test app spec
    
    * Load yaml spec
    
    * Create app if it doesn't exist
    
    * Update dist
    
    * Capture commands output
    
    * Make commands silent
    
    * Name workflow
    
    * Log if existing app found
    
    * Update dist
    
    * Prettify messages
    
    * Update dist
    
    * Output app-id, cleanup
    
    * Fix getting app-id after app creation
    
    * Improve logging
    
    * Update dist
    
    * Update readme

--- README.md
+++ README.md
@@ -1,34 +1,26 @@
 # DigitalOcean App Deploy action
 
-This action updates and deploys an app on DigitalOcean from an app spec file.
+This action updates and deploys an app on DigitalOcean from an app spec file using `doctl`.
 
 
 ## Usage
 
 ```yaml
-    - uses: jjst/action-digitalocean-deploy-app@v1
+    - uses: jjst/action-digitalocean-deploy-app@v2
       with:
         token: ${{ secrets.DIGITALOCEAN_ACCESS_TOKEN }}
-        application-id: <existing-application-id>
 ```
 
 ## Inputs
 
 - `token` – (**Required**) A DigitalOcean personal access token ([more info](https://www.digitalocean.com/docs/api/create-personal-access-token/)).
-- `application-id` - (**Required**) The application id of the existing, deployed application to update.
+- `app-id` - (Optional) The id of an existing, deployed application to update.
 - `spec` - (Optional) The path to the spec file to deploy. Defaults to `./deploy/app.yaml`.
 
-### Finding an App's application id and app spec
+If `app-id` is not supplied, then the action will look for an existing app with the same name as what is defined in the
+app spec file. If no such app is found, it will create a new app using the app spec. Otherwise, it will update the
+existing app with the app spec.
 
-To obtain the application id and spec of an existing app, the simplest method is to install and use [doctl](https://github.com/digitalocean/doctl), DigitalOcean's command line app. Find an app's application id with:
+## Outputs
 
-```sh
-doctl app list
-```
-
-Get the current application's app spec with:
-
-```sh
-doctl app spec get <application-id>
-```
-using the application id from the previous step.
+- `app-id` - The id of the app that was created or updated.
--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
+const yaml = require('js-yaml');
+const fs   = require('fs');
+
 require('action-doctl');
 
 Promise.retry = function(fn, times, delay) {
@@ -21,14 +24,52 @@ Promise.retry = function(fn, times, delay) {
     });
 };
 
+async function checkOutput(command, args) {
+  let output = '';
+
+  const options = {};
+  options.listeners = {
+    stdout: (data) => {
+      output += data.toString();
+    }
+  };
+  options.silent = true;
+  await exec.exec(command, args, options);
+  return output;
+}
+
+
 async function run() {
   try {
+    const specPath = core.getInput('spec', { required: true });
+    var applicationId = core.getInput('app-id');
     // Hack: wait for doctl to get set up
     await Promise.retry(() => exec.exec('doctl', ['apps', 'list'], {'silent': true}), 30, 1000);
-    var applicationId = core.getInput('application-id');
-    var specPath = core.getInput('spec');
-    await exec.exec('doctl', ['apps', 'update', applicationId, '--spec', specPath]);
-    await exec.exec('doctl', ['apps', 'create-deployment', applicationId, '--wait']);
+    if (!applicationId) {
+      core.info(">>> 'app-id' was not set, trying to determine app-id from spec name");
+      const deploySpec = yaml.load(fs.readFileSync(specPath, 'utf8'));
+      const deploySpecName = deploySpec['name']
+      core.debug("Deploy name: ", deploySpec['name']);
+      const appListStr = await checkOutput('doctl app list --no-header -o json');
+      const appList = JSON.parse(appListStr);
+      var existingApp = appList.find(app => app.spec.name == deploySpecName);
+      if (!existingApp) {
+        core.info(`>>> No existing app found with name '${deploySpecName}'; creating a new app.`)
+        const createdAppsStr = await checkOutput('doctl app create --spec ./test/test-app.yaml -o json');
+        const createdApps = JSON.parse(createdAppsStr);
+        existingApp = createdApps.find(app => app.spec.name == deploySpecName);
+        core.info(`>>> Successfully created new app with id '${existingApp.id}'.`)
+      } else {
+        core.info(`>>> Updating existing app with name '${deploySpecName}' and id '${existingApp.id}' using new app spec...`)
+        await exec.exec('doctl', ['app', 'update', applicationId, '--spec', specPath]);
+        core.info(`>>> Successfully updated app.`)
+      }
+      applicationId = existingApp.id
+    }
+    core.setOutput("app-id", applicationId);
+    core.info(`>>> Creating new deployment...`)
+    await exec.exec('doctl', ['app', 'create-deployment', applicationId, '--wait']);
+    core.info(`>>> Created deployment.`)
   } catch (error) {
     core.setFailed(error.message);
   }
